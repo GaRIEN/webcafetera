e0fe93c7430c9c0c0a63f8b62c1e9657f01c95c3
ahuamanpowercode <ahuaman@powercode.pe>
2024-03-27 14:42:51 -0500

    creacion de los modelos servicios

--- webcafetera/servicios/admin.py
+++ webcafetera/servicios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Service
 
 # Register your models here.
+
+class serviciosAdmin(admin.ModelAdmin):
+    readonly_fields=('created','update')
+
+admin.site.register(Service,serviciosAdmin)
--- webcafetera/servicios/models.py
+++ webcafetera/servicios/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Service(models.Model):
+    title=models.CharField(max_length=200, verbose_name='titulo')
+    subtitle=models.CharField(max_length=200, verbose_name='subtitulo')
+    content=models.CharField(verbose_name='contenido')
+    created=models.DateTimeField(auto_now_add=True)
+    update=models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name='servicio'
+        verbose_name='servicios'
+        
+    def __str__(self):
+        return self.title
