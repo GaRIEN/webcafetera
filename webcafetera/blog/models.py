3f7460af8c4743fa323466d276c56756b1f5dbbe
“GaRIEN” <gabrielhuaman683@gmail.com>
2024-03-30 10:14:01 -0500

    creacion de los modelos categoria y post en blog

--- webcafetera/blog/models.py
+++ webcafetera/blog/models.py
@@ -8,26 +8,26 @@ User = get_user_model()
 class Categoria(models.Model):
     name=models.CharField(max_length=100,verbose_name='nombre')
     created=models.DateTimeField(auto_now_add=True)
-    update=models.DateTimeField(auto_now=True)
+    updated=models.DateTimeField(auto_now=True)
     
     class Meta:
         verbose_name='categoria'
         verbose_name_plural='categorias'
         ordering=['-created']
         
-    def __srt__(self):
+    def __str__(self):
         return self.name
     
     
 class Post(models.Model):
     title=models.CharField(max_length=200, verbose_name='titulo')
     content=models.TextField(verbose_name='contenido')
-    pubicado=models.DateTimeField(verbose_name='fecha de publicacion',default=now())
+    publicado = models.DateTimeField(verbose_name='fecha de publicacion', default=now)
     image=models.ImageField(upload_to='blog', verbose_name='imagen',null=True, blank=True)
     autor=models.ForeignKey(User,verbose_name='Autor', on_delete=models.CASCADE)
     categoria=models.ManyToManyField(Categoria,verbose_name='categoria')
     created=models.DateTimeField(auto_now_add=True)
-    update=models.DateTimeField(auto_now=True)
+    updated=models.DateTimeField(auto_now=True)
     
     
     class Meta:
@@ -35,6 +35,6 @@ class Post(models.Model):
         verbose_name_plural='posts'
         ordering=['-created']
         
-    def __srt__(self):
+    def __str__(self):
         return self.title
     
